f03736768f8c3da7b71988b217e2814360b2941c
Darion Valdez <darionvldz@gmail.com>
2024-09-12 17:37:35 +0200

    add basic html structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <button>Audio</button>
+        <button>Settings</button>
+      </nav>
+      <main>
+        <div className="header-container">
+          <h1>Letter spinner</h1>
+        </div>
+        <div className="letter-container">?</div>
+        <div className="timer-container">00:00</div>
+        <div className="controls-container">Play</div>
+      </main>
+      <footer>Credits</footer>
+    </>
+  );
 }
 
 export default App;
