6afca3aaea95c5ee9f35ac6c2def81ba7bcc915d
Darion Valdez <darionvldz@gmail.com>
2024-09-12 20:58:16 +0200

    add spin letter function

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import "./index.css";
 
 function App({ letters }) {
+  const NR_OF_SPINS = 5;
+  const SPIN_SPEED_IN_MS = 100;
+
   const [currentLetter, setCurrentLetter] = useState("?");
+  const [isSpinning, setSpinning] = useState(false);
+
+  useEffect(() => {
+    console.log(currentLetter);
+  }, [currentLetter]);
+
+  useEffect(() => {
+    console.log(isSpinning);
+  }, [isSpinning]);
 
   function getRandomLetter() {
-    const randomIndex = Math.floor(Math.random() * letters.length);
+    let randomIndex = Math.floor(Math.random() * letters.length);
     setCurrentLetter(letters[randomIndex]);
+  }
+
+  function spinLetters() {
+    if (isSpinning) return;
+    setSpinning(true);
+    for (let i = 1; i <= NR_OF_SPINS; i++) {
+      setTimeout(() => {
+        getRandomLetter();
+        if (i === NR_OF_SPINS) {
+          setSpinning(false);
+        }
+      }, i * SPIN_SPEED_IN_MS);
+    }
   }
 
   return (
@@ -25,7 +50,7 @@ function App({ letters }) {
         </div>
         <div className="timer-container flex-center padding-large">00:00</div>
         <div className="controls-container flex-center padding-medium">
-          <button onClick={getRandomLetter}>Play</button>
+          <button onClick={spinLetters}>Play</button>
         </div>
       </main>
       <footer>Credits</footer>
