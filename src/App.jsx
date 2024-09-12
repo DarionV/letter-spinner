22ce5fd3fe29e9ab48f7f1795fe63d3897d3d4dd
Darion Valdez <darionvldz@gmail.com>
2024-09-12 21:53:54 +0200

    rewrite scramble letter logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,38 +2,61 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import "./index.css";
 
-function App({ letters }) {
-  const NR_OF_SPINS = 5;
+function App(props) {
+  const NR_OF_SPINS = 40;
   const SPIN_SPEED_IN_MS = 100;
 
   const [currentLetter, setCurrentLetter] = useState("?");
   const [isSpinning, setSpinning] = useState(false);
+  const [shouldSpin, setShouldSpin] = useState(false);
+  const [letters, setLetters] = useState(props.letters);
+
+  let controlButton = isSpinning ? (
+    ""
+  ) : (
+    <button onClick={spinLetters}>Play</button>
+  );
 
   useEffect(() => {
     console.log(currentLetter);
   }, [currentLetter]);
 
-  useEffect(() => {
-    console.log(isSpinning);
-  }, [isSpinning]);
+  // useEffect(() => {
+  //   console.log(isSpinning);
+  // }, [isSpinning]);
 
-  function getRandomLetter() {
-    let randomIndex = Math.floor(Math.random() * letters.length);
-    setCurrentLetter(letters[randomIndex]);
+  useEffect(() => {
+    console.log(letters);
+  }, [letters]);
+
+  function scrambleLetters() {
+    let scrambledLetters = [...letters].sort(() => {
+      return Math.random() - 0.5;
+    });
+    setLetters(scrambledLetters);
+    setShouldSpin(true);
   }
 
   function spinLetters() {
     if (isSpinning) return;
     setSpinning(true);
-    for (let i = 1; i <= NR_OF_SPINS; i++) {
+    scrambleLetters();
+  }
+
+  useEffect(() => {
+    if (!shouldSpin) return;
+
+    for (let i = 0; i < NR_OF_SPINS; i++) {
       setTimeout(() => {
-        getRandomLetter();
-        if (i === NR_OF_SPINS) {
+        if (i >= letters.length) setCurrentLetter(letters[i - letters.length]);
+        else setCurrentLetter(letters[i]);
+        if (i === NR_OF_SPINS - 1) {
           setSpinning(false);
+          setShouldSpin(false);
         }
       }, i * SPIN_SPEED_IN_MS);
     }
-  }
+  }, [shouldSpin, letters]);
 
   return (
     <>
@@ -50,7 +73,7 @@ function App({ letters }) {
         </div>
         <div className="timer-container flex-center padding-large">00:00</div>
         <div className="controls-container flex-center padding-medium">
-          <button onClick={spinLetters}>Play</button>
+          {controlButton}
         </div>
       </main>
       <footer>Credits</footer>
